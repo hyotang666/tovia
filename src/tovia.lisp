e720802d7313c59bac4957f8cd0352c61b8180e7
Shinichi Sato <hyotang.karakoma@gmail.com>
2021-04-15 15:12:17 +0900

    Add: HELLO-TOVIA

--- src/tovia.lisp
+++ src/tovia.lisp
@@ -16,6 +16,14 @@
 
 (defparameter *box-size* 16)
 
+(defparameter *title* "Top View Action.")
+
+(defvar *scene*
+  (lambda (win)
+    (error
+      "No scene bound for window ~S. ~:@_Do (setf tovia:*scene* your-func) or eval (tovia:main your-func)."
+      win)))
+
 (defun boxel () (* *box-size* *pixel-size*))
 
 ;;;; SEQUENCE-TRANSITION
@@ -34,28 +42,31 @@
 
 ;;;; MAIN
 
-(defun main ()
+(defun main (&optional (scene #'hello-tovia))
+  (setq *scene* scene)
   (uiop:nest
     (sdl2:with-init (:everything))
     (sdl2:with-window (win :flags '(:shown :opengl)
                            :w (* *width* (boxel))
                            :h (* *height* (boxel))
-                           :title "Top View Action."))
+                           :title *title*))
     (sdl2:with-gl-context (context win)
       (gl:enable :blend)
       (gl:blend-func :src-alpha :one-minus-src-alpha))
     (fude-gl:with-shader ())
     (fude-gl:with-textures ())
-    (sequence-handler-bind (fun #'test)
+    (sequence-handler-bind (fun *scene*)
       (funcall fun win))))
 
 ;;;; TRANSITIONS
 ;; TEST
 
-(defun test (win)
+(defun hello-tovia (win)
   (uiop:nest
+    (fude-gl:with-text-renderer (text :win win :size 32))
     (sdl2:with-event-loop (:method :poll)
       (:quit ()
         t))
     (:idle nil)
-    (fude-gl:with-clear (win (:color-buffer-bit)))))
+    (fude-gl:with-clear (win (:color-buffer-bit))
+      (text "Hello tovia." :x :center :y :center))))
